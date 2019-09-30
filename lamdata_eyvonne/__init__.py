c9f33fda8e63b10e47054a8161fa199e05efeeec
eyvonne <eyvonne@users.noreply.github.com>
2019-09-30 16:25:10 -0700

    added sklearn to the project

--- lamdata_eyvonne/__init__.py
+++ lamdata_eyvonne/__init__.py
@@ -3,6 +3,12 @@ a collection of helper functions for DS
 """
 import numpy as np
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
 ONES=pd.DataFrame(np.ones(10))
 ZEROS=pd.DataFrame(np.zeros(15))
+
+
+def traintest(df, df2):
+    train, test, tar, tartest=train_test_split(df, df2)
+    return tar
