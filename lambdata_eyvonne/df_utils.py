b241565bdf9d65d821f0a9e004185a668d0027ac
eyvonne <eyvonne@users.noreply.github.com>
2019-10-01 12:51:35 -0700

    update with class

---
+++ lambdata_eyvonne/df_utils.py
@@ -0,0 +1,57 @@
+"""
+utility functions for working with dataframes
+
+"""
+
+import pandas as pd
+
+TEST_DF = pd.DataFrame([1, 3, 4])
+
+
+def genData(DF, factor):
+    """DF is a dataframe to be generated off of
+    factor is how many addtional rows to create"""
+    df = pd.concat([DF, DF.sample(factor)])
+    return df
+
+
+def listCol(df, list, colName):
+    """
+    df is the dataframe
+    list is a list to be added to the df
+    colName is the name to use for the new column
+    """
+    ser = pd.Series(list)
+    if df.shape[0] == ser.shape[0]:
+        df[colName] = ser.copy()
+    else:
+        print('list does not have enough elements. Needs', df.shape[0])
+    return df
+
+
+class ManageDF():
+    """
+    A collection of helper functions for adding data or generating data
+    """
+
+    def __intit__(self, DF):
+        self.df = df
+
+    def listCol(self, list, colName):
+        """
+        df is the dataframe
+        list is a list to be added to the df
+        colName is the name to use for the new column
+        """
+        ser = pd.Series(list)
+        if self.df.shape[0] == ser.shape[0]:
+            df[colName] = ser.copy()
+        else:
+            print('list does not have enough elements. Needs', self.df.shape[0])
+        return self.df
+
+    def genData(self, factor):
+        """DF is a dataframe to be generated off of
+        factor is how many addtional rows to create"""
+        df = pd.concat([self.df, self.df.sample(factor)])
+        return df
